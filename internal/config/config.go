389656d6553cdc9be11465ec51cb55791944f26f
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2025-07-16 20:21:00 +0200

    implement config
    - read the config file stored on the disc
    - write the config file to the disc
    - set username

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,82 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
 type Config struct {
 	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+
+	configFile, err := getConfigFilePath()
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(configFile)
+
+	if err != nil {
+		return Config{}, err
+	}
+
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+
+	cfg := Config{}
+
+	if err := decoder.Decode(&cfg); err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
+}
+
+func getConfigFilePath() (string, error) {
+	homeLocation, err := os.UserHomeDir()
+
+	if err != nil {
+		return "", err
+	}
+
+	configFile := filepath.Join(homeLocation, configFileName)
+
+	return configFile, nil
+}
+
+func write(cfg Config) error {
+	configFile, err := getConfigFilePath()
+
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(configFile)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+
+	if err := encoder.Encode(cfg); err != nil {
+		return err
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+
+	"github.com/NikolaTosic-sudo/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+
+	if err != nil {
+		log.Fatalf("error reading config %v", err)
+	}
+
+	fmt.Printf("Read config: %v\n", cfg)
+
+	errName := cfg.SetUser("nikola")
+
+	if errName != nil {
+		log.Fatalf("error reading config %v", errName)
+	}
+
+	cfg, err = config.Read()
+
+	if err != nil {
+		log.Fatalf("error reading config %v", errName)
+	}
+
+	fmt.Printf("Read config again: %v\n", cfg)
+}
